c1b67a27cc3d7a80d0828ea4c872af729893cb61
SabineZilde <sabine.zilde@protonmail.ch>
2021-07-04 13:54:35 +0300

    Updating landing page

--- src/Components/Home.js
+++ src/Components/Home.js
@@ -16,7 +16,7 @@ function Home() {
         <div className="container">
             <div className="row">
                 <div className="col mt-3 pt-4 pb-2 text-center" style={{background: 'marbel'}}>
-                    <p style={{fontSize: '25px', lineHeight: '14px'}}><i>The Best <b>Art Supplies</b> at the Best Prices!</i></p>
+                    <p style={{fontSize: '30px', lineHeight: '14px'}}><b>The Best Art Supplies at the Best Prices!</b></p>
                     <p style={{lineHeight: '14px', fontSize:'14px'}}>Find all your art supply needs in one place! Shop more than 90,000 art supplies online, including the best selection of painting supplies, drawing supplies, and more.</p>
                 </div>
             </div>
